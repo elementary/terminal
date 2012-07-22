63831699be754e1d13d39f2f55dda2d87e700a9b
Mario Guerriero <mefrio.g@gmail.com>
2012-07-22 23:46:09 +0200

    fixed bug #1007080, now .desktop file is fully translatable

--- src/config.vala.cmake
+++ src/config.vala.cmake
@@ -6,4 +6,11 @@ public const string RELEASE_NAME = "@RELEASE_NAME@";
 public const string VERSION = "@VERSION@";
 public const string VERSION_INFO = "@VERSION_INFO@";
 public const string PLUGINDIR = "@PLUGINDIR@";
+/* Translatable launcher (.desktop) strings to be added to   */
+/* template (.pot) file. These strings should reflect any    */
+/* changes in these launcher keys in .desktop file.          */
+public const string COMMENT = N_("Use the command line");
+public const string GENERIC = N_("Terminal");
+public const string NEW_WINDOW = N_("New Window");
+public const string NEW_ROOT_WINDOW = N_("New Root Window");
 }
